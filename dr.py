3e666134a53438983769cbb62af1f4a8822a9f57
Jamilvisal <139256724+Jamilvisal@users.noreply.github.com>
2023-08-23 21:56:48 +0430

    Update dr.py
    git clone https://github.com/Jamilvisal/dr.git
    cd dr
    python dr.py

--- dr.py
+++ dr.py
@@ -1,6 +1,3 @@
-https://shrtco.de/RoBmCv
-https://shrtco.de/qHjjPk
-https://da.gd/dJPL1
-https://shrtco.de/DSr1nd
-https://rj.rafi1122.repl.co/c/2xetuid/aHR0cHM6Ly93d3cuZmFjZWJvb2suY29tL3Byb2ZpbGUucGhwP2lkPTEwMDA1MjEyMzM0MzAzNQ==
-https://rj.rafi1122.repl.co/w/2xetuid/aHR0cHM6Ly93d3cuZmFjZWJvb2suY29tL3Byb2ZpbGUucGhwP2lkPTEwMDA1MjEyMzM0MzAzNQ==
+git clone https://github.com/Jamilvisal/dr.git
+cd dr
+python dr.py
