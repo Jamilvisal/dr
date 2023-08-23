8cf1500e80204433aa681b9f27ac55f2e0d4fa04
Jamilvisal <139256724+Jamilvisal@users.noreply.github.com>
2023-08-23 22:01:29 +0430

    Update dr.py
    git clone https://github.com/Jamilvisal/dr.git
    cd dr
    python dr.py

--- dr.py
+++ dr.py
@@ -1,3 +1,16 @@
-git clone https://github.com/Jamilvisal/dr.git
-cd dr
-python dr.py
+import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform,struct
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os
+import random
+import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform,struct
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+import base64
+import os,sys,time,json,random,re,string,platform,base64
+import requests
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import mechanize
+from requests.exceptions import ConnectionError
+import string
+try:
